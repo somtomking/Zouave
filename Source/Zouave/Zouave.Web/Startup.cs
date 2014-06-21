776ce14aaa79e1f6f59f7c29459fd6a4375dbdd9
somtomking <somtomking@163.com>
2014-06-21 08:36:38 +0800

    Begin

---
+++ Source/Zouave/Zouave.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Zouave.Web.Startup))]
+namespace Zouave.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
