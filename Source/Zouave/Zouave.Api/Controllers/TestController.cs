0615fc97ccd3e4dcb4d6132457c871c2afd773f0
somtomking <somtomking@163.com>
2014-06-27 18:01:09 +0800

    api

--- Source/Zouave/Zouave.Api/Controllers/TestController.cs
+++ Source/Zouave/Zouave.Api/Controllers/TestController.cs
@@ -4,21 +4,34 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Zouave.Api.Models;
 
 namespace Zouave.Api.Controllers
 {
+
     public class TestController : ApiController
     {
-        // GET api/test
-        public IEnumerable<string> Get()
+
+        List<TestModel> _data = new List<TestModel>();
+
+        public TestController()
         {
-            return new string[] { "value1", "value2" };
+            for (int loop = 0; loop < 10; loop++)
+            {
+                _data.Add(new TestModel { Id = loop, IsActive = (loop % 2) == 0, Name = loop.ToString(), Price = loop * 2.54M });
+            }
+        }
+
+        // GET api/test
+        public IEnumerable<TestModel> Get()
+        {
+            return _data;
         }
 
         // GET api/test/5
-        public string Get(int id)
+        public TestModel Get(int id)
         {
-            return "value";
+            return _data.Find(s => s.Id == id);
         }
 
         // POST api/test
--- Source/Zouave/Zouave.Api/Global.asax.cs
+++ Source/Zouave/Zouave.Api/Global.asax.cs
@@ -12,6 +12,7 @@ namespace Zouave.Api
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            
         }
     }
 }
