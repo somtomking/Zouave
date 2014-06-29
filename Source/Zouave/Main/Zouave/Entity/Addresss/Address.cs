736493c3b2e18f46bcb33933658fb977641f52f7
somtomking <somtomking@163.com>
2014-06-29 11:54:07 +0800

    base

---
+++ Source/Zouave/Main/Zouave/Entity/Addresss/Address.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zouave.Entity.Addresss
+{
+    public class Address : Entity
+    {
+        public string CountryCode { get; set; }
+        public string ProvinceCode { get; set; }
+        public string CityCode { get; set; }
+        public string StreetCode { get; set; }
+        public string Detail { get; set; }
+
+    }
+}
---
+++ Source/Zouave/Main/Zouave/Entity/Common/Dictionary.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Zouave.Entity.Common
+{
+    public class Dictionary : Entity
+    {
+        public string Code { get; set; }
+        public string ParentCode { get; set; }
+        public string Value { get; set; }
+        public string Scope { get; set; }
+    }
+}
