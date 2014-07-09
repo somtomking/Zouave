aae3af9488ba8293a4620189964fc561b5884cbc
somtomking <somtomking@163.com>
2014-07-09 15:07:03 +0800

    加入RMB和拼音的类

---
+++ Source/Zouave/Main/Zouave.Test/Helper/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zouave.Helper;
+
+namespace Zouave.Test.Helper
+{
+    [TestClass]
+    public class PinyinHelperTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var result = PinyinHelper.Convert("您好啊~");
+            
+        }
+    }
+}
