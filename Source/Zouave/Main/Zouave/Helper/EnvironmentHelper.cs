09a99c5c9010b1a1840dd0e7a1390122bbb50a60
somtomking <somtomking@163.com>
2014-07-10 13:39:42 +0800

    获取当前程序占用的内存大小(兆)

---
+++ Source/Zouave/Main/Zouave/Helper/EnvironmentHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zouave.Helper
+{
+    public static class EnvironmentHelper
+    {
+        /// <summary>
+        /// 获取当前程序占用的内存大小(兆)
+        /// </summary>
+        /// <returns></returns>
+        public static long GetCurrentMemorySize()
+        {
+            var result = Process.GetCurrentProcess().PrivateMemorySize64 / 1024 / 1024;
+            return result;
+        }
+    }
+}
