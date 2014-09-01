aa52837a2fa5ef2c46b8ca4c4727db124350b2c2
somtomking <somtomking@163.com>
2014-09-01 21:13:51 +0800

    dodo

--- Source/Zouave/Web/Zouave.Web/App_Start/RouteConfig.cs
+++ Source/Zouave/Web/Zouave.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace Zouave.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new string[] { "Zouave.Web.Controllers" }
             );
         }
     }
