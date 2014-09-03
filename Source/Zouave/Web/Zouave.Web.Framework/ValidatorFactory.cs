a93c4ab29405f4bebd5674d62b1554efbb96c15a
somtomking <somtomking@163.com>
2014-09-03 16:57:35 +0800

    代码提交

---
+++ Source/Zouave/Web/Zouave.Web.Framework/ValidatorFactory.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using FluentValidation.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zouave.Infrastructure;
+
+namespace Zouave.Web.Framework
+{
+    public class ValidatorFactory : AttributedValidatorFactory
+    {
+        //private readonly InstanceCache _cache = new InstanceCache();
+        public override IValidator GetValidator(Type type)
+        {
+            if (type != null)
+            {
+                var attribute = (ValidatorAttribute)Attribute.GetCustomAttribute(type, typeof(ValidatorAttribute));
+                if ((attribute != null) && (attribute.ValidatorType != null))
+                {
+                    //validators can depend on some customer specific settings (such as working language)
+                    //that's why we do not cache validators
+                    //var instance = _cache.GetOrCreateInstance(attribute.ValidatorType,
+                    //                           x => EngineContext.Current.ContainerManager.ResolveUnregistered(x));
+                    var instance = EngineContext.Current.ContainerManager.ResolveUnregistered(attribute.ValidatorType);
+                    return instance as IValidator;
+                }
+            }
+            return null;
+
+        }
+    }
+}
--- Source/Zouave/Web/Zouave.Web/Global.asax.cs
+++ Source/Zouave/Web/Zouave.Web/Global.asax.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using FluentValidation.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Zouave.Infrastructure;
+using Zouave.Web.Framework;
+using Zouave.Web.Framework.Mvc;
 
 namespace Zouave.Web
 {
@@ -12,9 +16,25 @@ namespace Zouave.Web
     {
         protected void Application_Start()
         {
+            //initialize engine context
+            EngineContext.Initialize(false);
+
+            //model binders
+            ModelBinders.Binders.Add(typeof(BaseNopModel), new ZouaveModelBinder());
+
+            //Add some functionality on top of the default ModelMetadataProvider
+            ModelMetadataProviders.Current = new ZouaveMetadataProvider();
+
             AreaRegistration.RegisterAllAreas();
+
+
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            //fluent validation
+            DataAnnotationsModelValidatorProvider.AddImplicitRequiredAttributeForValueTypes = false;
+            ModelValidatorProviders.Providers.Add(new FluentValidationModelValidatorProvider(new ValidatorFactory()));
+
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             
         }
