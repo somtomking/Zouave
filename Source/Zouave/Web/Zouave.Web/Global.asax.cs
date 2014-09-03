aba29ffb055680d231a23fa60b20804722aa8cd3
somtomking <somtomking@163.com>
2014-09-03 17:16:18 +0800

    代码提交

--- Source/Zouave/Web/Zouave.Web/Global.asax.cs
+++ Source/Zouave/Web/Zouave.Web/Global.asax.cs
@@ -1,11 +1,15 @@
 ﻿using FluentValidation.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Zouave.Data;
+using Zouave.Framework;
 using Zouave.Infrastructure;
 using Zouave.Web.Framework;
 using Zouave.Web.Framework.Mvc;
@@ -36,7 +40,135 @@ namespace Zouave.Web
             ModelValidatorProviders.Providers.Add(new FluentValidationModelValidatorProvider(new ValidatorFactory()));
 
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+
+            bool databaseInstalled = DataSettingsHelper.DatabaseIsInstalled();
+            if (databaseInstalled)
+            {
+
+            }
             
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            //ignore static resources
+            var webHelper = EngineContext.Current.Resolve<IWebHelper>();
+            if (webHelper.IsStaticResource(this.Request))
+                return;
+
+            //keep alive page requested (we ignore it to prevent creating a guest customer records)
+            string keepAliveUrl = string.Format("{0}keepalive/index", webHelper.GetStoreLocation());
+            if (webHelper.GetThisPageUrl(false).StartsWith(keepAliveUrl, StringComparison.InvariantCultureIgnoreCase))
+                return;
+
+            //ensure database is installed
+            if (!DataSettingsHelper.DatabaseIsInstalled())
+            {
+                string installUrl = string.Format("{0}install", webHelper.GetStoreLocation());
+                if (!webHelper.GetThisPageUrl(false).StartsWith(installUrl, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    this.Response.Redirect(installUrl);
+                }
+            }
+
+            if (!DataSettingsHelper.DatabaseIsInstalled())
+                return;
+
+            
+        }
+
+        protected void Application_EndRequest(object sender, EventArgs e)
+        {
+          
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+            //we don't do it in Application_BeginRequest because a user is not authenticated yet
+            SetWorkingCulture();
+        }
+
+        protected void Application_Error(Object sender, EventArgs e)
+        {
+            var exception = Server.GetLastError();
+
+            //log error
+            LogException(exception);
+
+            //process 404 HTTP errors
+            var httpException = exception as HttpException;
+            if (httpException != null && httpException.GetHttpCode() == 404)
+            {
+                var webHelper = EngineContext.Current.Resolve<IWebHelper>();
+                if (!webHelper.IsStaticResource(this.Request))
+                {
+                    Response.Clear();
+                    Server.ClearError();
+                    Response.TrySkipIisCustomErrors = true;
+
+                
+                }
+            }
+        }
+
+        protected void SetWorkingCulture()
+        {
+            if (!DataSettingsHelper.DatabaseIsInstalled())
+                return;
+
+            //ignore static resources
+            var webHelper = EngineContext.Current.Resolve<IWebHelper>();
+            if (webHelper.IsStaticResource(this.Request))
+                return;
+
+            //keep alive page requested (we ignore it to prevent creation of guest customer records)
+            string keepAliveUrl = string.Format("{0}keepalive/index", webHelper.GetStoreLocation());
+            if (webHelper.GetThisPageUrl(false).StartsWith(keepAliveUrl, StringComparison.InvariantCultureIgnoreCase))
+                return;
+
+
+            if (webHelper.GetThisPageUrl(false).StartsWith(string.Format("{0}admin", webHelper.GetStoreLocation()),
+                StringComparison.InvariantCultureIgnoreCase))
+            {
+                //admin area
+
+
+                //always set culture to 'en-US'
+                //we set culture of admin area to 'en-US' because current implementation of Telerik grid 
+                //doesn't work well in other cultures
+                //e.g., editing decimal value in russian culture
+                CommonHelper.SetTelerikCulture();
+            }
+            else
+            {
+                
+            }
+        }
+
+        protected void LogException(Exception exc)
+        {
+            if (exc == null)
+                return;
+
+            if (!DataSettingsHelper.DatabaseIsInstalled())
+                return;
+
+            //ignore 404 HTTP errors
+            var httpException = exc as HttpException;
+            //if (httpException != null && httpException.GetHttpCode() == 404 &&
+            //    !EngineContext.Current.Resolve<CommonSettings>().Log404Errors)
+            //    return;
+
+            try
+            {
+                //log
+                
+            }
+            catch (Exception)
+            {
+                //don't throw new exception if occurs
+            }
         }
     }
 }
