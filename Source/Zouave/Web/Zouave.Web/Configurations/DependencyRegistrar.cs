1a255c6cdca557be5f1df13dff88182209524c40
somtomking <somtomking@163.com>
2014-09-03 17:20:29 +0800

    常规提交

---
+++ Source/Zouave/Web/Zouave.Web/Configurations/DependencyRegistrar.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Zouave.Infrastructure.DependencyManagement;
+
+namespace Zouave.Web.Configurations
+{
+    public class DependencyRegistrar : IDependencyRegistrar
+    {
+
+        public void Register(ContainerBuilder builder, Infrastructure.ITypeFinder typeFinder)
+        {
+            builder.RegisterControllers(System.Reflection.Assembly.GetExecutingAssembly());
+        }
+
+        public int Order
+        {
+            get { return 999; }
+        }
+    }
+}
