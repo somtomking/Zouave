a8267ec764a891e697e995db300a319dc95d7ccc
somtomking <somtomking@163.com>
2014-09-03 15:25:16 +0800

    常规代码更新

---
+++ Source/Zouave/Web/Zouave.Web/Configurations/DependencyInjectionMvcModule.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Zouave.Web.Configurations
+{
+    public class DependencyInjectionMvcModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterControllers(System.Reflection.Assembly.GetExecutingAssembly());
+
+        }
+    }
+}
